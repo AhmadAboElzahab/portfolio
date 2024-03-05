be49ed0db3ed65729946d7df110533e2236a3757
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-05 18:03:08 +0400

    adding images folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/Navbar';
+import Mypicture from './components/Mypicture';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className='text-white bg-black'>
         <Navbar />
+        <Mypicture />
         {children}
       </body>
     </html>
