f994ed3540b22fc3c9101c6546c7fd99d5c2fd3d
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-05 18:22:36 +0400

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import Image from 'next/image';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className=" text-center">
-      <h1 className="text-5xl">Hey its Ahmad Here</h1>
+    <main className=' text-center'>
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant='outline'>Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </main>
   );
 }
