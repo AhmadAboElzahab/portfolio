2bde00d220307e471c3d9e6e1e462ff16570d99b
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-07 12:11:50 +0400

    add active links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,53 @@
+'use client';
+
 import Link from 'next/link';
 import Logo from './Logo';
+import { usePathname } from 'next/navigation';
+
 export default function Navbar() {
+  const NavLinks = [
+    { id: 1, name: 'Home', path: '/' },
+    { id: 2, name: 'Blog', path: '/blog' },
+    { id: 3, name: 'About', path: '/about' },
+    { id: 4, name: 'Contact', path: '/contact' },
+  ];
+  const pathname = usePathname();
+  const isActive = (path: string) => path === pathname;
+
   return (
-    <nav className=' border-b-2 border-white  rsticky drop-shadow-xl z-10'>
+    <nav className='  sticky drop-shadow-xl z-10'>
       <div className='mx-auto flex justify-between flex-col sm:flex-row'>
-        <div className='mx-auto text-white w-[55%] flex felx-row justify-between p-5'>
+        <div className='mx-auto fon text-[#727279]  font-medium	w-[55%] flex felx-row justify-between p-3'>
           <div>
-            <h1 className='text-white no-underline '>
+            <h1 className=' no-underline pt-3'>
               <Link href='/'>
                 <Logo />
               </Link>
             </h1>
           </div>
-          <div className='bg-red-200  flex gap-x-4'>
-            <Link href='/'>Home</Link>
-            <Link href='/contact'>Blog</Link>
-            <Link href='/about'>About</Link>
-            <Link href='/contact'>Contact</Link>
+          <div className='flex gap-x-2 '>
+            <div className='pt-1 flex gap-x-2 '>
+              {NavLinks.map((link) => {
+                return (
+                  <>
+                    <Link
+                      className={` ${
+                        isActive(link.path) ? 'text-white ' : ''
+                      }hover:text-white hover:transition duration-300`}
+                      href={link.path}
+                    >
+                      {link.name}
+                    </Link>
+                  </>
+                );
+              })}
+            </div>
+            <Link
+              className='bg-[#8E8E98] hover:bg-[#aaaab1] py-1 px-2 hover:transition duration-300	text-black  rounded-sm '
+              href='/contact'
+            >
+              Resume
+            </Link>
           </div>
         </div>
       </div>
