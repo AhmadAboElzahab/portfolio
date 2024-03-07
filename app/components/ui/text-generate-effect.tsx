4f611cdefee59b60aaaf59d333f1470488c5822f
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-07 20:58:41 +0400

    fixing colors

--- app/components/ui/text-generate-effect.tsx
+++ app/components/ui/text-generate-effect.tsx
@@ -24,7 +24,7 @@ export const TextGenerateEffect = ({ words, className }: { words: string; classN
       <motion.div ref={scope}>
         {wordsArray.map((word, idx) => {
           return (
-            <motion.span key={word + idx} className='text-white opacity-0'>
+            <motion.span key={word + idx} className='text-[#727279] opacity-0'>
               {word}{' '}
             </motion.span>
           );
@@ -34,7 +34,7 @@ export const TextGenerateEffect = ({ words, className }: { words: string; classN
   };
 
   return (
-    <div className={cn('font-bold', className)}>
+    <div className={cn(className)}>
       <div className='mt-4'>
         <div className='text-xl text-left leading-snug tracking-wide'>{renderWords()}</div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -11,29 +11,31 @@ export default function Home() {
       <BigText />
 
       <div className='h-[50rem] w-full bg-black bg-dot-white/[0.2] relative flex items-center justify-center'>
-        <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
         <div className='flex flex-col'>
-          <motion.div
-            initial={{ opacity: 0, x: -300 }}
-            viewport={{ once: true }}
-            whileInView={{ opacity: 1, x: 300, transition: { duration: 1.0 } }}
-          >
-            <div className='text-5xl'>Who am I</div>
-          </motion.div>
           <motion.div
             initial={{ opacity: 0, x: -300 }}
             viewport={{ once: true }}
             whileInView={{ opacity: 1, x: 0, transition: { duration: 1.0 } }}
           >
-            <TextGenerateEffect
-              words='    Passionate software engineer with 3+ years of expertise in crafting immersive and
+            <div className='text-5xl text-center'>Who am I</div>
+          </motion.div>
+          <div className='z-10'>
+            <motion.div
+              initial={{ opacity: 0, x: -300 }}
+              viewport={{ once: true }}
+              whileInView={{ opacity: 1, x: 0, transition: { duration: 1.0 } }}
+            >
+              <TextGenerateEffect
+                words='Passionate software engineer with 3+ years of expertise in crafting immersive and
             responsive websites. Proficient in JavaScript Frameworks, and a suite of tools. I blend
             innovation with functionality to deliver seamless user experiences. A collaborative
             spirit fuels my agile approach, partnering with diverse teams to create digital
             landscapes that resonate with both clients and end-users.'
-            />
-          </motion.div>
+              />
+            </motion.div>
+          </div>
         </div>
+        <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] z-0'></div>
       </div>
     </main>
   );
