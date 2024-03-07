6cc9e7e3152428e66ff68df6de12184d4db5c518
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-07 11:19:01 +0400

    add logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import Link from 'next/link';
+import Logo from './Logo';
 export default function Navbar() {
   return (
-    <nav className='border-b-2 border-white  rsticky drop-shadow-xl z-10'>
-      <div className=' mx-auto flex justify-between flex-col sm:flex-row'>
-        <Link href='/' className='no-underline text-4xl text-white'>
-          Home
-        </Link>
-        <Link href='/contact' className='no-underline text-4xl text-white'>
-          Blog
-        </Link>
-        <Link href='/about' className='no-underline text-4xl text-white'>
-          About
-        </Link>
-        <Link href='/contact' className='no-underline text-4xl text-white'>
-          Contact
-        </Link>
+    <nav className=' border-b-2 border-white  rsticky drop-shadow-xl z-10'>
+      <div className='mx-auto flex justify-between flex-col sm:flex-row'>
+        <div className='mx-auto text-white w-[55%] flex felx-row justify-between p-5'>
+          <div>
+            <h1 className='text-white no-underline '>
+              <Link href='/'>
+                <Logo />
+              </Link>
+            </h1>
+          </div>
+          <div className='bg-red-200  flex gap-x-4'>
+            <Link href='/'>Home</Link>
+            <Link href='/contact'>Blog</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/contact'>Contact</Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
