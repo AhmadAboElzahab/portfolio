5d042205c21e1186f6963e39f7b10ea189eb9720
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-07 13:09:54 +0400

    fix font size

--- app/components/BigText.tsx
+++ app/components/BigText.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function BigText() {
   return (
-    <div className='mt-[17%] '>
-      <h1 className='text-8xl font-extrabold text-white'>Ahmad Abo Elzahab</h1>
+    <div className='mt-[35%] md:mt-[25%] lg:mt-[17%] '>
+      <h1 className='text-5xl md:text-6xl lg:text-8xl font-extrabold text-white'>
+        Ahmad Abo Elzahab
+      </h1>
     </div>
   );
 }
