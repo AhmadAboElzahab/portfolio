dc1bf7fe44090bb7baad1562da32286e2289f7a2
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-09 14:16:35 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['0.0.0.0', '*'],
+    },
+  },
+};
 
 export default nextConfig;
