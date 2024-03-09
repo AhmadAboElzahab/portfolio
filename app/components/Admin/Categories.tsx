e6732e9985c04dc0baffe61b56be02a324b901aa
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-09 15:13:43 +0400

    add optimisticCategories

---
+++ app/components/Admin/Categories.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useOptimistic, useRef } from 'react';
+import { useFormStatus } from 'react-dom';
+export default function Categories({ addCategory, categories }: any) {
+  const { pending } = useFormStatus();
+  const ref = useRef<HTMLFormElement>(null);
+  const [optimisticCategories, addOptimisticCategory] = useOptimistic(
+    categories,
+    (state, newCategory: any) => {
+      return [...state, newCategory];
+    },
+  );
+
+  return (
+    <>
+      <form
+        ref={ref}
+        action={async (formData) => {
+          ref.current?.reset();
+          addOptimisticCategory({
+            id: Math.random(),
+            title: formData.get('title') as string,
+          });
+
+          await addCategory(formData);
+        }}
+        className='text-red-500'
+      >
+        <input type='text' name='title' />
+        <input disabled={pending} type='submit' value='add' />
+      </form>
+      {optimisticCategories.map((category: any) => (
+        <div key={category.id}>{category.title}</div>
+      ))}
+    </>
+  );
+}
--- app/dashboard/blog/page.tsx
+++ app/dashboard/blog/page.tsx
@@ -2,17 +2,14 @@
 import React from 'react';
 import { prisma } from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
-import AddPost from '@/app/components/Admin/AddPost';
+import AddPost from '@/app/components/Admin/Categories';
 import { addCategory } from '@/actions/actions';
 export default async function Blog() {
   const categories = await prisma.category.findMany();
 
   return (
     <div>
-      <AddPost addCategory={addCategory} />
-      {categories.map((category) => (
-        <div key={category.id}>{category.title}</div>
-      ))}
+      <AddPost addCategory={addCategory} categories={categories} />
     </div>
   );
 }
