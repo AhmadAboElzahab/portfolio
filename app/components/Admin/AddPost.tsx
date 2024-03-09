fd4c6697225ed9e23a35ba762cd3a85830a43788
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-09 14:00:40 +0400

    fixing bugs

--- app/components/Admin/AddPost.tsx
+++ app/components/Admin/AddPost.tsx
@@ -2,12 +2,7 @@
 import React from 'react';
 export default function AddPost({ addCategory }: any) {
   return (
-    <form
-      action={async (formData) => {
-        await addCategory(formData);
-      }}
-      className='text-red-500'
-    >
+    <form action={addCategory} className='text-red-500'>
       <input type='text' name='title' />
       <input type='submit' value='add' />
     </form>
