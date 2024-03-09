4a3b9a6a9561fce706a12c03e14892074e7998bb
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-09 12:32:59 +0400

    fixing bugs

--- app/dashboard/blog/page.tsx
+++ app/dashboard/blog/page.tsx
@@ -1,13 +1,32 @@
 import { prisma } from '@/lib/prisma';
 import React from 'react';
-import AddPost from '@/app/components/Admin/AddPost';
+import { revalidatePath } from 'next/cache';
 
 export default async function Blog() {
   const categories = await prisma.category.findMany();
- 
+  const addCategory = async (formData: FormData) => {
+    'use server';
+    const title = formData.get('title');
+
+    try {
+      await prisma.category.create({
+        data: {
+          title: title as string,
+          description: 'Posts related to technology',
+        },
+      });
+      revalidatePath('/dashboard/blog');
+    } catch (error) {
+      console.error('Error creating category:', error);
+      // Handle errors appropriately, e.g., display error message to user
+    }
+  };
   return (
     <div>
-      <AddPost />
+      <form action={addCategory} className='text-red-500'>
+        <input type='text' name='title' />
+        <input type='submit' value='add' />
+      </form>
       {categories.map((category) => (
         <div key={category.id}>{category.title}</div>
       ))}
