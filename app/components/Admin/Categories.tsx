f74605b29f4a892c7647347e84520a987c185bcb
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-10 19:02:28 +0400

    category tables

--- app/components/Admin/Categories.tsx
+++ app/components/Admin/Categories.tsx
@@ -1,44 +1,86 @@
 'use client';
-
 import { useOptimistic, useRef } from 'react';
 import { useFormStatus } from 'react-dom';
+import { VscEdit } from 'react-icons/vsc';
+import { RxCross2 } from 'react-icons/rx';
+
+type category = {
+  id: number;
+  title: string;
+  type: string;
+};
+type categories = {
+  categories: category[];
+};
 export default function Categories({ addCategory, categories }: any) {
   const { pending } = useFormStatus();
   const ref = useRef<HTMLFormElement>(null);
   const [optimisticCategories, addOptimisticCategory] = useOptimistic(
     categories,
-    (state, newCategory: any) => {
+    (state: any, newCategory: category) => {
       return [...state, newCategory];
     },
   );
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    ref.current?.reset();
+    addOptimisticCategory({
+      id: Math.random(),
+      title: formData.get('title') as string,
+      type: formData.get('type') as string,
+    });
+    await addCategory(formData);
+  };
+
   return (
-    <div className='w-[25%] '>
+    <div className='w-[25%]'>
       <div className='border-grayBorder border-[1px] bg-darkGray mb-[0.75rem] rounded-lg p-5'>
         <h1 className='text-2xl font-bold'>Add Category</h1>
-        <form
-          ref={ref}
-          action={async (formData) => {
-            ref.current?.reset();
-            addOptimisticCategory({
-              id: Math.random(),
-              title: formData.get('title') as string,
-              type: formData.get('type') as string,
-            });
 
-            await addCategory(formData);
-          }}
-          className='text-red-500'
-        >
-          <input type='text' name='title' />
+        <form onSubmit={handleSubmit}>
+          <input type='text' name='title' className='' />
+          <input type='text' name='type' />
 
           <input disabled={pending} type='submit' value='add' />
         </form>
       </div>
-      <div className='w-full border-grayBorder border-[1px] bg-darkGray rounded-lg p-5 '>
-        {optimisticCategories.map((category: any) => (
-          <div key={category.id}>{category.title + ' ' + category.type}</div>
-        ))}
+      <div className='border-grayBorder border-[1px] bg-darkGray rounded-lg p-3'>
+        <table className='w-full'>
+          <thead>
+            <tr>
+              <th className='text-left'>Title</th>
+              <th>Type</th>
+              <th>Delete</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody className='text-xs'>
+            {optimisticCategories.map((category: category) => (
+              <tr key={category.id}>
+                <td className='text-left'>{category.title}</td>
+                <td>
+                  <p
+                    className={` w-fit mx-auto text-center  rounded-xl px-2 py-1 ${
+                      category.type === 'Frontend' ? 'text-[#BF7AF0] bg-[#2F1939]' : ''
+                    } ${category.type === 'Backend' ? 'text-[#FF5733] bg-[#33130A]' : ''} ${
+                      category.type === 'General' ? 'text-[#3498DB] bg-[#154360]' : ''
+                    }`}
+                  >
+                    {category.type}
+                  </p>
+                </td>
+                <td className='text-center'>
+                  <RxCross2 className='mx-auto text-lg' />
+                </td>
+                <td className='text-center'>
+                  <VscEdit className='mx-auto text-center text-lg' />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,39 +42,6 @@ const config = {
       colors: {
         darkGray: '#0A0A0A',
         grayBorder: '#252525',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
       },
     },
   },
