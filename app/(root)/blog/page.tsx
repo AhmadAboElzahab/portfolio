a9886f407706c4790feac4c3aa873de79c3c995d
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-03-20 15:13:46 +0400

    add loading

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -12,13 +12,13 @@ export default async function Page() {
   });
 
   return (
-    <div className='grid grid-cols-3 w-[65%] mx-auto gap-10 pt-20 min-h-screen pb-40'>
+    <div className='grid grid-cols-1 md:grid-cols-3 w-[90%]  md:w-[65%] mx-auto gap-10 pt-20 min-h-screen pb-40'>
       {posts.map((p: any, i: number) => (
         <Link href={`/blog/${p.category.type}/${p.category.title}/${p.id}`} key={i}>
           <div className='rounded-md hover:scale-105 transition-all duration-300 border-[1px] border-grayBorder'>
             <div
               className='rounded-md'
-              style={{ position: 'relative', width: '100%', height: '279px' }}
+              style={{ position: 'relative', width: '100%', height: '200px' }}
             >
               <Image src={p.image} fill alt='' className='blur-md' style={{ objectFit: 'cover' }} />
               <Image
@@ -29,16 +29,11 @@ export default async function Page() {
                 style={{ objectFit: 'cover' }}
               />
             </div>
-            <div className='hover:text-white text-white/35 px-2 py-1 backdrop-blur-sm w-full rounded-b-md'>
-              <h1 className='h-9'>{p.title}</h1>
+            <div className='pt-2 text-white/35 px-2 py-1 backdrop-blur-sm w-full rounded-b-md'>
+              <h1 className='text-white h-11 font-bold pb-3'>{p.title}</h1>
               <p className='text-right text-sm'>
                 {formatDistanceToNow(new Date(p.date), { addSuffix: true })}
               </p>
-              {p.category && (
-                <div className='text-xs mt-1'>
-                  Category: <strong>{p.category.title}</strong> ({p.category.type})
-                </div>
-              )}
             </div>
           </div>
         </Link>
