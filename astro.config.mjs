d7791be0da42b935f3cec1e575aa576ecc649436
Ahmad Abo Elzahab <server.vbs@gmail.com>
2025-08-10 20:02:28 +0400

    Add initial wrangler configuration for Cloudflare Worker project
    - Created a new wrangler.jsonc file to configure the Worker.
    - Set project name to "portfolio" and specified main entry point.
    - Defined compatibility date and flags for enhanced functionality.
    - Configured asset binding and observability settings.
    - Included documentation links for further configuration guidance.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+
+    imageService: "cloudflare",
+  }),
+});
