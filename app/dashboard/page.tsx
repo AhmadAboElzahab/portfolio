76dae3cab1e7db2fcb273e269fbb7dc2bd88004b
Ahmad Abo Elzahab <server.vbs@gmail.com>
2025-08-10 18:57:23 +0400

    Remove project files and configurations for a complete reset, including package.json, PostCSS, Prisma migrations, Tailwind CSS configuration, TypeScript configuration, utility functions, and seed data.

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ahmad() {
-  return <div>ahmad</div>;
-}
--- prisma/migrations/20240320165038_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `descreption` to the `Post` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "descreption" TEXT NOT NULL;
